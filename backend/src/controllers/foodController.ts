b7e7fbdd04d4a31585e27dd10a4335f3835bbb08
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-08 19:05:49 +0300

    finished meal and ingredient creation controllers

--- backend/src/controllers/foodController.ts
+++ backend/src/controllers/foodController.ts
@@ -1,7 +1,7 @@
 import { ctrlWrapper } from 'decorators';
 import { db } from 'drizzle';
 import { and, eq, sql } from 'drizzle-orm';
-import { ingredients, meals } from 'drizzle/schema';
+import { ingredients, meals, mealsIngredients } from 'drizzle/schema';
 import { RequestHandler } from 'express';
 import { HttpError } from 'helpers';
 
@@ -101,9 +101,52 @@ const getIngredientById: RequestHandler = async (req, res) => {
   res.json(ingredientById);
 };
 
-const createMeal: RequestHandler = async (req, res) => {};
+const createMeal: RequestHandler = async (req, res) => {
+  const { name, description, nutrients, type, category, ingredients } =
+    req.body;
 
-const createIngredient: RequestHandler = async (req, res) => {};
+  const [meal] = await db.select().from(meals).where(eq(meals.name, name));
+  if (meal) {
+    throw HttpError(409, 'Meal with the same name already exists');
+  }
+
+  const [newMeal] = await db
+    .insert(meals)
+    .values({ name, description, nutrients, type, category })
+    .returning();
+
+  if (!newMeal) {
+    throw HttpError(400, 'Failed to create meal');
+  }
+
+  const mealIngredientEntries = ingredients.map(
+    ({ ingredientId, measurement }: any) => ({
+      mealId: newMeal.id,
+      ingredientId,
+      measurement,
+    })
+  );
+
+  await db.insert(mealsIngredients).values(mealIngredientEntries);
+
+  res.json({ ...req.body });
+};
+
+const createIngredient: RequestHandler = async (req, res) => {
+  const { name } = req.body;
+
+  const [ingredient] = await db
+    .select()
+    .from(ingredients)
+    .where(eq(ingredients.name, name));
+  if (ingredient) {
+    throw HttpError(409, 'Ingredient with the same name already exists');
+  }
+
+  await db.insert(ingredients).values({ ...req.body });
+
+  res.json({ ...req.body });
+};
 
 export default {
   getMeals: ctrlWrapper(getMeals),
