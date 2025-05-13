d8a368915f2cb9f7c5cab86643f7da1dbd6dc716
Karr4 but from another universe <XGon20987@gmail.com>
2025-05-13 19:06:15 +0300

    added mobile menu

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { MobileMenu } from "src/components/common/MobileMenu";
 
 export const NavBar = () => {
   const pathname = usePathname();
@@ -15,60 +16,76 @@ export const NavBar = () => {
     signIn: "/sign-in",
   };
 
+  const isLoggedIn = localStorage.getItem("token");
+
   return (
-    <header className="py-[16px] flex justify-between items-center">
-      <nav>
+    <header className="py-[16px]">
+      <nav className="flex justify-between items-center">
         <Link href="/">
           <p className="font-semibold text-base md:text-lg">
             <span className="text-emerald-500">diet</span>plan.
           </p>
         </Link>
 
-        <div className="h-full flex items-center justify-center gap-[16px]">
-          <Link
-            href="/"
-            className={`text-sm ${
-              pathname === paths.home ? "text-emerald-500" : "text-black"
-            }`}
-          >
-            Home
-          </Link>
-          <Link
-            href="/generate-meal-plan"
-            className={`text-sm ${
-              pathname === paths.generateMealPlan
-                ? "text-emerald-500"
-                : "text-black"
-            }`}
-          >
-            Generate meal plan
-          </Link>
-          <Link
-            href="/meals"
-            className={`text-sm ${
-              pathname === paths.meals ? "text-emerald-500" : "text-black"
-            }`}
-          >
-            Meals
-          </Link>
-          <Link
-            href="/ingredients"
-            className={`text-sm ${
-              pathname === paths.ingredients ? "text-emerald-500" : "text-black"
-            }`}
-          >
-            Ingredients
-          </Link>
-        </div>
+        <ul className="sm:hidden h-full flex items-center justify-center gap-[16px]">
+          <li>
+            <Link
+              href="/"
+              className={`text-sm ${
+                pathname === paths.home ? "text-emerald-500" : "text-black"
+              }`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/generate-meal-plan"
+              className={`text-sm ${
+                pathname === paths.generateMealPlan
+                  ? "text-emerald-500"
+                  : "text-black"
+              }`}
+            >
+              Generate meal plan
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/meals"
+              className={`text-sm ${
+                pathname === paths.meals ? "text-emerald-500" : "text-black"
+              }`}
+            >
+              Meals
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/ingredients"
+              className={`text-sm ${
+                pathname === paths.ingredients
+                  ? "text-emerald-500"
+                  : "text-black"
+              }`}
+            >
+              Ingredients
+            </Link>
+          </li>
+        </ul>
 
-        <div className="flex gap-2">
-          <Link href={paths.signIn} className="text-base">
-            Sign In
-          </Link>
-          <Link href={paths.signup} className="text-base">
-            Signup
-          </Link>
-        </div>
+        <MobileMenu />
+
+        {!isLoggedIn && (
+          <div className="sm:hidden md:block flex gap-2">
+            <Link href={paths.signIn} className="text-base">
+              Sign In
+            </Link>
+            <Link href={paths.signup} className="text-base">
+              Signup
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
---
+++ frontend/src/components/common/MobileMenu.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="md:hidden">
+      <button onClick={toggleIsOpen}>
+        <svg
+          width="28"
+          height="28"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+        >
+          <path
+            fill="#000000"
+            d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"
+          />
+        </svg>
+      </button>
+
+      {isOpen && (
+        <div className="absolute inset-0 bg-gray-800 pt-[56px] px-[16px] pb-[16px] flex flex-col justify-between">
+          <button
+            onClick={toggleIsOpen}
+            className="absolute top-[16px] right-[16px]"
+          >
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+              fill="#ffffff"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 5L5 19M5.00001 5L19 19"
+                stroke="#ffffff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+
+          <ul className="flex flex-col gap-[16px]">
+            <Link
+              href="/"
+              className="font-medium text-xl text-emerald-500"
+              onClick={toggleIsOpen}
+            >
+              Home
+            </Link>
+            <Link
+              href="/generate-meal-plan"
+              className="font-medium text-xl text-emerald-500"
+              onClick={toggleIsOpen}
+            >
+              Generate meal plan
+            </Link>
+            <Link
+              href="/meals"
+              className="font-medium text-xl text-emerald-500"
+              onClick={toggleIsOpen}
+            >
+              Meals
+            </Link>
+            <Link
+              href="/ingredients"
+              className="font-medium text-xl text-emerald-500"
+              onClick={toggleIsOpen}
+            >
+              Ingredients
+            </Link>
+          </ul>
+
+          <div className="flex flex-col items-center gap-2">
+            <Link
+              href="/signup"
+              className="px-[36px] py-[8px] rounded-lg bg-emerald-500 text-white"
+              onClick={toggleIsOpen}
+            >
+              Signup
+            </Link>
+            <Link
+              href="sign-in"
+              className="text-base text-emerald-500"
+              onClick={toggleIsOpen}
+            >
+              Already a member? Sign In
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
