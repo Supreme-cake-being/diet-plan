d44c7a099fc437729c37219c510d246f6744e67f
Karr4 but from another universe <XGon20987@gmail.com>
2025-05-30 18:12:52 +0300

    Update page.tsx

--- frontend/app/meals/page.tsx
+++ frontend/app/meals/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { MealsList } from "src/components/MealsList";
 
-export default async function GenerateMealPlanPage() {
+export default async function MealsPage() {
   const { data } = await axios.get(process.env.NEXT_PUBLIC_API + "/food/meals");
 
   return <MealsList meals={data} />;
